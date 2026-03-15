8c1ea70e592f4778ea9c17e94ec78f8c6ec1b8e3
PRITHIKA PALANISAMY <prithika6868@gmail.com>
2026-03-15 16:31:34 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,13 @@
+import React from "react";
+import UrlChecker from "./UrlChecker";
 
+function App() {
+  return (
+    <div style={{textAlign:"center",marginTop:"50px"}}>
+      <h1>PhishGuard</h1>
+      <UrlChecker />
+    </div>
+  );
+}
+
+export default App;
