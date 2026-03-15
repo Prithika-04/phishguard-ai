e8e69aa13034903c423ac2adb918c5cabc595707
PRITHIKA PALANISAMY <prithika6868@gmail.com>
2026-03-15 16:21:56 +0530

    Add URL checking endpoint with request model

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from model import predict_url
 
 app = FastAPI()
 
+class URLRequest(BaseModel):
+    url: str
+
 @app.get("/")
 def home():
-    return {"message": "PhishGuard API Running"}
+    return {"message": "PhishGuard API running"}
+
+@app.post("/check")
+def check_url(data: URLRequest):
+    result = predict_url(data.url)
+    return {"result": result}
